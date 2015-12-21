2d17fdd7ba4a59f5fed92588ff9777827f6ef2dc
Kevin Desai <kevindesai777@gmail.com>
2015-12-21 01:57:39 -0500

    Initial Commit

---
+++ app/src/main/java/buildbetterapps/caption/CaptionApp.java
@@ -0,0 +1,13 @@
+package buildbetterapps.caption;
+import android.app.Application;
+
+/**
+ * Created by KevinDesai on 12/18/15.
+ */
+public class CaptionApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
+
---
+++ app/src/main/java/buildbetterapps/caption/MainActivity.java
@@ -0,0 +1,343 @@
+package buildbetterapps.caption;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.afollestad.assent.Assent;
+import com.afollestad.assent.AssentCallback;
+import com.afollestad.assent.PermissionResultSet;
+import com.clarifai.api.ClarifaiClient;
+import com.clarifai.api.RecognitionRequest;
+import com.clarifai.api.RecognitionResult;
+import com.clarifai.api.Tag;
+import com.clarifai.api.exception.ClarifaiException;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+
+import org.w3c.dom.Text;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private int PICK_IMAGE_REQUEST = 1;
+    private ImageView imageView;
+    private EditText textView;
+    private TextView selectImage;
+    private static final String APP_ID = "Iyyadv-rZs3xtlk_5QVnPd-fmSSNuSwQUGigKYVs";
+    private static final String APP_SECRET = "vA5htLCtjrGceYcREcgsXx7_g2Ymebt9_tAah_Pi";
+    private final ClarifaiClient client = new ClarifaiClient(APP_ID,APP_SECRET );
+    private Button copyClip;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Assent.setActivity(this, this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        textView = (EditText) findViewById(R.id.textView);
+        selectImage = (TextView) findViewById(R.id.inputImage);
+        copyClip = (Button) findViewById(R.id.copyClipboard);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                if (!Assent.isPermissionGranted(Assent.READ_EXTERNAL_STORAGE) && !Assent.isPermissionGranted(Assent.WRITE_EXTERNAL_STORAGE)) {
+                    Assent.requestPermissions(new AssentCallback() {
+                        @Override
+                        public void onPermissionResult(PermissionResultSet result) {
+                            // Permission granted or denied
+                            if (result.isGranted(Assent.READ_EXTERNAL_STORAGE) && result.isGranted(Assent.WRITE_EXTERNAL_STORAGE)){
+                                Intent intent = new Intent();
+                                // Show only images, no videos or anything else
+                                intent.setType("image/*");
+                                intent.setAction(Intent.ACTION_GET_CONTENT);
+                                // Always show the chooser (if there are multiple options available)
+                                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+                            }
+                            else{
+                                Snackbar.make(view, "Need access to storage to select pictures.", Snackbar.LENGTH_LONG)
+                                    .setAction("Action", null).show();
+                            }
+                        }
+                    }, 69, Assent.READ_EXTERNAL_STORAGE, Assent.WRITE_EXTERNAL_STORAGE);
+                }
+                else{
+                    Intent intent = new Intent();
+                    // Show only images, no videos or anything else
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    // Always show the chooser (if there are multiple options available)
+                    startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+                }
+
+
+            }
+
+
+
+        });
+
+        copyClip.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(final View view) {
+                ClipboardManager clipboard = (ClipboardManager)
+                        getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("all captions",textView.getText());
+                clipboard.setPrimaryClip(clip);
+                Snackbar.make(view, "Text copied to clipboard.", Snackbar.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Updates the activity every time the Activity becomes visible again
+        Assent.setActivity(this, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Cleans up references of the Activity to avoid memory leaks
+        if (isFinishing())
+            Assent.setActivity(this, null);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // Lets Assent handle permission results and contact your callbacks
+        Assent.handleResult(permissions, grantResults);
+    }
+
+    //Picasso/Glide, Okhttp, material dialogs, butterknife(optional) permissions
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+            String realPath = getImageUrlWithAuthority(getApplicationContext(), uri);
+            Uri realUri = Uri.parse(realPath);
+            Bitmap bitmap = loadBitmapFromUri(realUri);
+
+            if(bitmap != null)
+            {
+                selectImage.setVisibility(View.INVISIBLE);
+                imageView.setVisibility(View.VISIBLE);
+                //textView.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
+                imageView.setImageBitmap(bitmap);
+
+
+
+
+                new AsyncTask<Bitmap, Void, RecognitionResult>() {
+                    @Override protected RecognitionResult doInBackground(Bitmap... bitmaps) {
+                        return recognizeBitmap(bitmaps[0]);
+                    }
+                    @Override protected void onPostExecute(RecognitionResult result) {
+                        updateUIForResult(result);
+                    }
+                }.execute(bitmap);
+
+            }
+            else {
+                textView.setText("Locha");
+            }
+
+
+            //passImagetoServer(getImageUrlWithAuthority(getApplicationContext(), uri));
+        }
+    }
+
+    /*public String getImagePath(Uri uri){
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":")+1);
+        cursor.close();
+
+        cursor = getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
+
+        return path;
+    }
+
+
+    public static Uri handleImageUri(Uri uri) {
+        Pattern pattern = Pattern.compile("(content://media/.*\\d)");
+        if (uri.getPath().contains("content")) {
+            Matcher matcher = pattern.matcher(uri.getPath());
+            if (matcher.find())
+                return Uri.parse(matcher.group(1));
+            else
+                throw new IllegalArgumentException("Cannot handle this URI");
+        } else
+            return uri;
+    }*/
+
+
+    private Bitmap loadBitmapFromUri(Uri uri) {
+        try {
+            // The image may be large. Load an image that is sized for display. This follows best
+            // practices from http://developer.android.com/training/displaying-bitmaps/load-bitmap.html
+            BitmapFactory.Options opts = new BitmapFactory.Options();
+            opts.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(getContentResolver().openInputStream(uri), null, opts);
+            int sampleSize = 1;
+            while (opts.outWidth / (2 * sampleSize) >= imageView.getWidth() &&
+                    opts.outHeight / (2 * sampleSize) >= imageView.getHeight()) {
+                sampleSize *= 2;
+            }
+
+            opts = new BitmapFactory.Options();
+            opts.inSampleSize = sampleSize;
+            return BitmapFactory.decodeStream(getContentResolver().openInputStream(uri), null, opts);
+        } catch (IOException e) {
+            //Log.e(TAG, "Error loading image: " + uri, e);
+        }
+        return null;
+    }
+
+
+    public static String getImageUrlWithAuthority(Context context, Uri uri) {
+        InputStream is = null;
+        if (uri.getAuthority() != null) {
+            try {
+                is = context.getContentResolver().openInputStream(uri);
+                Bitmap bmp = BitmapFactory.decodeStream(is);
+                return writeToTempImageAndGetPathUri(context, bmp).toString();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }finally {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static Uri writeToTempImageAndGetPathUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+
+    private RecognitionResult recognizeBitmap(Bitmap bitmap) {
+        try {
+            // Scale down the image. This step is optional. However, sending large images over the
+            // network is slow and  does not significantly improve recognition performance.
+            Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 320,
+                    320 * bitmap.getHeight() / bitmap.getWidth(), true);
+
+            // Compress the image as a JPEG.
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            scaled.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            byte[] jpeg = out.toByteArray();
+
+            // Send the JPEG to Clarifai and return the result.
+            return client.recognize(new RecognitionRequest(jpeg)).get(0);
+        } catch (ClarifaiException e) {
+            //Log.e(TAG, "Clarifai error", e);
+            return null;
+        }
+    }
+
+    private void updateUIForResult(RecognitionResult result) {
+        if (result != null) {
+            if (result.getStatusCode() == RecognitionResult.StatusCode.OK) {
+                // Display the list of tags in the UI.
+                StringBuilder b = new StringBuilder();
+                for (Tag tag : result.getTags()) {
+                    b.append(b.length() > 0 ? "" : "").append("#").append(tag.getName());
+                }
+                textView.setText(b);
+                progressBar.setVisibility(View.INVISIBLE);
+                textView.setVisibility(View.VISIBLE);
+                copyClip.setVisibility(View.VISIBLE);
+
+            } else {
+                //Log.e(TAG, "Clarifai: " + result.getStatusMessage());
+                textView.setText("Sorry, there was an error recognizing your image.");
+            }
+        } else {
+            textView.setText("Sorry, there was an error recognizing your image.");
+        }
+        //selectButton.setEnabled(true);
+    }
+
+}
